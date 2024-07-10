b5a1555ab774f93df57b784efd9fea78a86ec687
Arne Claus <arne.claus@trivago.com>
2024-07-10 16:19:24 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "required tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, ...} @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+        {
+          formatter = pkgs.alejandra;
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              pre-commit
+              shellcheck
+              just
+            ];
+          };
+        }
+    );
+}
